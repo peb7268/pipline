e683137a11e413bbad176143479c1cc05fad764b
peb7268 <peb7268@gmail.com>
2015-01-15 01:34:49 -0500

    router working w/o browserify

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -1,21 +1,20 @@
-'use strict';
-
-require('angular');
-require('angular-route');
-require('angular-animate');
-require('angular-resource');
-
-var controllers = require('./controllers/AppController.js');
-
-var App = angular.module('App', ['ngRoute', 'AppController']);
-
-App.config(['$routeProvider'], function($routeProvider){
+window.App = angular.module('App', ['ngRoute', 'appControllers']).config(['$routeProvider',
+function($routeProvider){
     $routeProvider.
-        when('/hello', {
-            templateUrl: '../views/pages/hello',
-            controller: 'AppController'
-        }).otherwise({
-            redirectTo: '/hello'
-        });
-});
+    when('/hello', {
+        templateUrl: './views/pages/hello.html',
+        controller: 'AppCtrl'
+    }).
+    when('/login', {
+        templateUrl: './views/pages/login.html',
+        controller: 'LoginCtrl'
+    }).
+    when('/register', {
+        templateUrl: './views/pages/register.html',
+        controller: 'RegisterCtrl'
+    })
+    .otherwise({
+        redirectTo: '/hello'
+    });
+}]);
 
--- www/public/js/controllers/AppController.js
+++ www/public/js/controllers/AppController.js
@@ -1,6 +1,16 @@
-exports.AppController = (function(){
-    console.log('loaded');
-    return {
+var appControllers = angular.module('appControllers', []);
 
-    };
-});
+appControllers.controller('AppCtrl', ['$scope',
+    function($scope){
+        console.log('AppCtrl');
+}]);
+
+appControllers.controller('LoginCtrl', ['$scope',
+    function($scope){
+        console.log('LoginCtrl');
+}]);
+
+appControllers.controller('RegisterCtrl', ['$scope',
+    function($scope){
+        console.log('RegisterCtrl');
+}]);
