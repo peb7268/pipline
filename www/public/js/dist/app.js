6fe516f2451107f0ff4ed27f826aa7cf191de025
peb7268 <peb7268@gmail.com>
2015-01-28 23:11:58 -0500

    working menu now

--- www/public/js/dist/app.js
+++ www/public/js/dist/app.js
@@ -10180,7 +10180,13 @@ var RegisterController          = require('./RegisterController');
 var controllers = {};
 
 controllers.AppController = function($scope){
-    var self = this;
+    this.toggleMenu = function(){
+    	var $el = $(event.target);
+    	if(event.target.nodeName != 'I') $el = $el.find('i');
+    	$el.toggleClass('open');
+
+    	$('#nav ul').slideToggle(100);
+    };
 };
 
 controllers.CalendarController  = CalendarController;
