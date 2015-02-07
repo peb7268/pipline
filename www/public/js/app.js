8274401f535c519545fd0da1f90651a400f0e523
peb7268 <peb7268@gmail.com>
2015-02-07 14:55:07 -0500

    setting endpoint dynamically now

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -1,6 +1,13 @@
 window.jQuery           = require('jquery');
 window.$                = window.jQuery;
 var angular             = require('angular');
+
+//Temp mock, later will bind the uid to the view while logged in or put it in local storage
+window.App = (typeof App != 'undefined') ? App : {};
+var App = window.App;
+App.user = {};
+App.user.id = 7;
+
 var App = require('./controllers');
 
 module.exports = App;
--- www/public/js/services/CalendarService.js
+++ www/public/js/services/CalendarService.js
@@ -1,8 +1,74 @@
 var CalendarService = function($http){
-    var items = [];
-    var self  = this;
+    var items   = [];
+    var self    = this;
+    var evt;
 
-    var request = $http.get('/api/v1/todos');
+    var user_id = App.user.id;
+    var year    = getYear(evt, window.location);
+    var month   = getMonth(evt, window.location);
+    var day     = getDay(evt, window.location);
+
+    var endpoint =  constructEndpoint();
+
+    var request = $http.get(endpoint);
+
+    /***
+    * Constructs an endpoint for fetching todos
+     * params window.location, evt
+    * */
+    function constructEndpoint(event, loc){
+        var endpoint         = '/api/v1/todos';
+        if(typeof user_id   != undefined) endpoint += '/' + user_id;
+        if(typeof year      != undefined) endpoint += '/' + year;
+        if(typeof month     != undefined) endpoint += '/' + month;
+        if(typeof day       != undefined) endpoint += '/' + day;
+
+        console.log(endpoint);
+        debugger;
+        return endpoint;
+    };
+
+    function retrieveDateFromUrl(loc, part){
+        var part;
+
+        switch(part){
+            case 'year':
+                debugger;
+            break
+        }
+
+        return part;
+    }
+
+    /**
+     * Get the year from the event if a click occurred else get the current year.
+     */
+    function getYear(evt){
+        var year;
+        if(typeof evt != 'undefined' && evt.type == 'click') year = retrieveDateFromEvent(evt, 'year');
+        year = new Date().getUTCFullYear();
+        if(typeof year == 'undefined') { throw new Error('Oops, Couldn\'t resolve year.'); return false; }
+
+        return year;
+    }
+
+    function getMonth(evt){
+        var mo;
+        if(typeof evt != 'undefined' && evt.type == 'click') mo = retrieveDateFromEvent(evt, 'month');
+        mo = (new Date().getUTCMonth() + 1);
+        if(typeof mo == 'undefined'){ throw new Error('Oops, Couldn\'t resolve month.'); return false; }
+
+        return mo;
+    }
+
+    function getDay(evt){
+        var day;
+        if(typeof evt != 'undefined' && evt.type == 'click') day = retrieveDateFromEvent(evt, 'day');
+        day = new Date().getUTCDate();
+        if(typeof day == 'undefined'){ throw new Error('Oops, Couldn\'t resolve month.'); return false; }
+
+        return day;
+    }
 
     return {
         req: request,
